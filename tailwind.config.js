03e45d277fae185d66604cb3e82ef2e9f2b87998
Ather1230 <ather.biswas0057@gmail.com>
2022-11-12 09:06:06 +0600

    banner add

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,25 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctorTheme: {
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: '#3A4256',
+          "base-100": '#FAFAFA',
+        }
+      }
+    ]
+
+  },
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        bannerImage: "url('/src/assets/images/bg.png')"
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
