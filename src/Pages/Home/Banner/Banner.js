ada882faec319944e3352e32060c5df154037172
Ather1230 <ather.biswas0057@gmail.com>
2022-11-12 10:50:36 +0600

    info added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
           className="rounded-lg shadow-2xl w-1/2"
         alt=""/>
         <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
