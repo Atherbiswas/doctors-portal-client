0cefb24b7b46ced31320958f5c09fc11fc73fe7d
Ather1230 <ather.biswas0057@gmail.com>
2022-11-12 16:09:59 +0600

    site logo added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -27,7 +27,7 @@ const Footer = () => {
       </div>
       </div>
       <div className="text-center mt-28">
-        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+        <p>Copyright © 2022 - All right reserved by <span className="font-bold">DOCTORS PORTAL.</span></p>
         </div>
     </footer>
   );
