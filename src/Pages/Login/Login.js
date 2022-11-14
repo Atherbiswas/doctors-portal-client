f880c4f6b5e3ad4143c781cc88b6f382cd4944a7
Ather1230 <ather.biswas0057@gmail.com>
2022-11-14 16:22:53 +0600

    login form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-           <h2>This is login page</h2> 
-        </div>
-    );
+    const {register, handleSubmit} = useForm();
+
+    const handleLogin = data => {
+        console.log(data)
+    }
+    
+  return (
+    <div className="h-[600px] flex justify-center items-center">
+      <div className="w-96 p-7 shadow-2xl rounded">
+        <h2 className="text-2xl font-bold text-center">Login!!</h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
+            <div className="form-control w-full max-w-xs">
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input type="email"  {...register("email")}  className="input input-bordered w-full max-w-xs"/>
+            </div>
+            <div className="form-control w-full max-w-xs">
+                <label className="label">
+                    <span className="label-text">Password</span>
+                </label>
+                <input type="password"  {...register("password")}  className="input input-bordered w-full max-w-xs"/>
+                <label className="label">
+                    <span className="label-text">Forget Password</span>
+                </label>
+            </div>
+          <input className="btn btn-accent w-full" value='Login' type="submit" />
+        </form>
+        <p>New to Doctors portal? <Link to='/signup' className="text-secondary">Create a new account</Link> </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline btn-accent w-full">CONTINUE WITH GOOGLE</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
